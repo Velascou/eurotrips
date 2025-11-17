28f58442288a1150d375ba3fe68a9d86cd2e0223
Velascouu <diego.velper.2@gmail.com>
2025-11-17 02:25:17 +0100

    Remove .vercel from repo and ignore it

--- .vercel/output/_functions/chunks/db_BnTjIUEm.mjs
+++
@@ -1,9 +0,0 @@
-import pkg from 'pg';
-
-const { Pool } = pkg;
-const connectionString = "postgresql://neondb_owner:npg_QHKO0h1uEbDI@ep-calm-rain-ab04vpsb-pooler.eu-west-2.aws.neon.tech/eurotrips?sslmode=require&channel_binding=require";
-const pool = new Pool({
-  connectionString
-});
-
-export { pool as p };
